0dc281016b8819576af96596118ccb790be8088e
TiagoNovelli <tiagonovelli95@gmail.com>
2022-02-18 10:51:48 -0300

    Salvando um arvivo simples de historico de testes do R

---
+++ linguagem-R/historico com algumas observações.R
@@ -0,0 +1,166 @@
+library(e1071)
+source("~/.active-rstudio-document", echo=TRUE)
+install.packages("e1071")
+library("e1071")
+library(e1071)
+plot(iris[,1:3])
+library(e1071)
+plot(iris[,1:4])
+plot(iris[,1:4])
+iris2 <- iris
+View(iris2)
+getwd()
+knitr::opts_chunk$set(echo = TRUE)
+x = c(14,18,23,25,27)
+y = c(1,3,6,9)
+plot(x,y)
+x = c(14,18,23,25,27)
+y = c(1,3,6,9,12)
+plot(x,y)
+install.packages("e1071")
+library("e1071")
+```{r}
+x = c(14,18,23,25,27)
+y = c(1,3,6,9,12)
+```
+ability.com
+install.packages("e1071")
+install.packages("e1071")
+library("e1071")
+```{r}
+x = c(14,18,23,25,27)
+y = c(1,3,6,9,12)
+```
+ability.cov
+knitr::opts_chunk$set(echo = TRUE)
+parouimpar = function(x)
+{
+  Ifelse(%/%2=0,"par", "impar")
+  parouimpar = function(x)
+  {
+    Ifelse(x%/%2=0,"par", "impar")
+    parouimpar = function(x)
+    {
+      Ifelse(x%/%2==0,"par","impar")
+      )
+parouimpar = function(x)
+{
+  Ifelse(x%/%2==0,"par","impar")
+}
+parouimpar(2)
+parouimpar = function(x)
+{
+  Ifelse(x%/%2==0,"par","impar")
+}
+parouimpar(2)
+parouimpar = function(x)
+{
+  return(Ifelse(x%/%2==0,"par","impar"))
+}
+parouimpar(2)
+parouimpar = function(x){
+  return(ifelse(x%/%2==0,"par","impar"))
+}
+parouimpar(2)
+parouimpar = function(x){
+  return(ifelse(x%/%2!=0,"par","impar"))
+}
+parouimpar(2)
+parouimpar = function(x){
+  return(ifelse(x%/%2!=0,"par","impar"))
+}
+parouimpar(1)
+parouimpar = function(x){
+  return(ifelse(x%/%2!=0,"par","impar"))
+}
+parouimpar(1001)
+parouimpar = function(x){
+  return(ifelse(x%%2==0,"par","impar"))
+}
+parouimpar(1001)
+parouimpar = function(x){
+  return(ifelse(x%%2==0,"par","impar"))
+}
+parouimpar(1002)
+parouimpar = function(x){
+  ifelse(x%%2==0,"par","impar")
+}
+parouimpar(1002)
+parouimpar = function(x){
+  ifelse(x%%2==0,"par","impar")
+}
+parouimpar(2002)
+parouimpar = function(x){
+  ifelse(x < 10,"menor q 10","maior q 10")
+}
+parouimpar(2002)
+parouimpar = function(x){
+  ifelse(x < 10,"menor q 10","maior q 10")
+}
+parouimpar(9)
+parouimpar = function(x){
+  ifelse(x < 10,"menor q 10","maior q 10")
+}
+parouimpar(11)
+source("~/.active-rstudio-document", encoding = 'UTF-8', echo=TRUE)
+source("~/.active-rstudio-document", encoding = 'UTF-8', echo=TRUE)
+class(BOD)
+tail(women,n=10)
+deznumeros = c(1:10)
+deznumeros = c(1:10)
+deznumeros = c(1:10)
+print(deznumeros)
+for(i%%2==0 in deznumeros)
+  for(i in deznumeros)
+    print(i)
+for(i in deznumeros)
+  ifelse(i%%2==0,print(i))
+for(i in deznumeros)
+  ifelse(deznumeros%%2==0,print(i))
+for(i in deznumeros)
+  if(deznumeros%%2==0,print(i))
+    for(i in deznumeros)
+      if(i%%2==0,print(i))
+        deznumeros = c(1:10)
+pares = deznumeros%%2==0
+print(pares)
+deznumeros = c(1:10)
+pares = deznumeros%%2==0
+print(pares.value)
+deznumeros = c(1:10)
+pares = c(deznumeros%%2==0L)
+print(pares)
+deznumeros = c[1:10]
+pares = deznumeros%%2==0
+print(pares)
+deznumeros = (1:10)
+While(deznumeros%%2==0){
+  print(deznumeros)
+  deznumeros = c(1:10)
+  While(deznumeros%%2==0){
+    print(deznumeros)
+    deznumeros = c(1:10)
+    pares = deznumeros%%2==0
+    print(pares)
+    pares = deznumeros%%2==0
+    mutate(pares=as.factor(pares))
+    print(pares)
+    deznumeros = c(1:10)
+    list(deznumeros%%2==0)
+    print(list)
+    deznumeros = c(1:10)
+    print(deznumeros)
+    deznumeros = c(1:10)
+    print(deznumeros%%2==0)
+    deznumeros = c(1:10)
+    #print(deznumeros%%2==0)
+    for(i in deznumeros)
+      print(i)
+    deznumeros = c(1:10)
+    #print(deznumeros%%2==0)
+    for(i in deznumeros)
+    {
+      if(i%%2==0)
+        print(i)
+    }
+    
